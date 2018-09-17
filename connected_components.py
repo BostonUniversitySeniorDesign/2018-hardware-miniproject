7c1974e7057c90dc3d3e6b29bdae3e3c70ef7928
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-17 11:18:26 -0400

    Update connected_components.py

--- connected_components.py
+++ connected_components.py
@@ -3,14 +3,28 @@
 setup of connected components with OpenCV (also possible with scikit-image)
 """
 import cv2
+import h5py
+from pathlib import Path
+from datetime import datetime
+
 
 def main():
 
     B = setupblob(params)
     
-    for img in imgs:
+    Ncount = []
+    for i, img in enumerate(imgs):
         motion = process(img)
         keypoints = B.detect(motion)
+        
+        Ncount.append(len(keypoints))
+        
+        if i and not i % 10:
+            countfn = PATH / ('count' + datetime.now().isoformat()[:-7] + '.h5')
+            with h5py.File(countfn, 'w') as f:
+                f['count'] = Ncount
+                f['index'] = i
+            Ncount = []
 
 
 def setupblob(param: dict):
